bafa97b8504a2ac028930418a79ba34387459e26
Harsh Dewan <harsh.dewan@outlook.com>
2024-05-03 13:56:13 -0400

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import HomePage from './components/HomePage/HomePage.jsx'
 function App()  {
     return(
         <>
-        <h1>This is app</h1>
         <HomePage />
         </>
     );
 }
-
 
 export default App
--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,11 +1,23 @@
+import './education-style.css';
+
 function Education() {
     return(
         <>
-        <br />
-        <h1>Education</h1>
-        Master of Applied Computing, University of Windsor Sep 2023 – Dec 2024 | Windsor, Canada
-        <br />
-        Bachelor of Computer Engineering, Thapar University Jul 2016 – Jul 2020 | Patiala, India
+        <div className="education">
+            <h1 className='education-heading'>Education</h1>
+            <div className='education-details'>
+                <div className='education-masters'>
+                    <p className='education-course'>Master of Applied Computing</p>
+                    <p className='education-university'>University of Windsor</p>
+                    <p className='education-duration'>Spetember 2023 -  December 2024</p>
+                </div>
+                <div className='education-bachelors'>
+                    <p className='education-course'>Bachelor of Computer Engineering</p>
+                    <p className='education-university'>Thapar Institute of Engineering and Technology</p>
+                    <p className='education-duration'>July 2016 -  July 2020</p>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
