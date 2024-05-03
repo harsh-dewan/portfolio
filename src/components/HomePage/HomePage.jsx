ddf891ea3ce0767eea9f3a0b93ca5a5f3bbeda58
Harsh Dewan <harsh.dewan@outlook.com>
2024-05-03 02:06:12 -0400

    project-structure-changes

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import './homepage-style.css'
+
+function HomePage() {
+
+    return(
+        <>
+        <header className="homepage-header">
+            <h1>Header</h1>
+        </header>
+        <main className="homepage-main">
+            <h1>Main</h1>
+        </main>
+        <footer className="homepage-footer">
+            <h1>Footer Section</h1>
+        </footer>
+        </>
+    );
+
+}
+
+
+export default HomePage;
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
+import './style.css'
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementsByClassName('root')[0]).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
