19b20e82112f2daa3673e65892d5b61e2289b87f
Harsh Dewan <harsh.dewan@outlook.com>
2024-05-05 00:19:49 -0400

    contact, experience and project changes

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -3,15 +3,17 @@ import './education-style.css';
 function Education() {
     return(
         <>
-        <div className="education">
-            <h1 className='education-heading'>Education</h1>
+        <div className="education-container">
+            <div className='education-heading'>
+                <h1>Education</h1>
+            </div>
             <div className='education-details'>
-                <div className='education-  '>
+                <div className='education'>
                     <p className='education-course'>Master of Applied Computing</p>
                     <p className='education-university'>University of Windsor</p>
-                    <p className='education-duration'>Spetember 2023 -  December 2024</p>
+                    <p className='education-duration'>September 2023 -  December 2024</p>
                 </div>
-                <div className='education-bachelors'>
+                <div className='education'>
                     <p className='education-course'>Bachelor of Computer Engineering</p>
                     <p className='education-university'>Thapar Institute of Engineering and Technology</p>
                     <p className='education-duration'>July 2016 -  July 2020</p>
@@ -22,3 +24,11 @@ function Education() {
     );
 }
 export default Education;
+
+/*
+<div className='education'>
+    <p className='education-course'>Bachelor of Computer Engineering</p>
+    <p className='education-university'>Thapar Institute of Engineering and Technology</p>
+    <p className='education-duration'>July 2016 -  July 2020</p>
+</div>
+*/
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,25 +4,30 @@ function Projects() {
     return(
         <>
         <div className='projects'>
-            <h1 className='project-title'>Projects</h1>
+           <div  className='project-title'>
+                <h1>Projects</h1>
+           </div>
             <div className='project-container'>
                 <div className='project-details'>
-                    <p>Custom Command Client-Server System Jan 2024 – Apr 2024</p>
-                    <p>Technology: C, Socket Programming</p>
-                    <p>Developed a robust client-server system using socket programming for seamless communication, with customized commands for efficient interaction</p>
-                    <p>Demonstrated strong problem-solving skills and collaboration in implementing and deploying a scalable, reliable system with error handling mechanisms</p>
+                    <p className='project-heading'>Custom Command Client-Server System (Jan 2024 - Apr 2024)</p>
+                    <p className='project-tech'>Technology: C, Socket Programming</p>
+                    <p className='project-description'>Developed a robust client-server system using socket programming for seamless communication, with customized commands for efficient interaction</p>
+                    <p className='project-description'>Demonstrated strong problem-solving skills and collaboration in implementing and deploying a scalable, reliable system with error handling mechanisms</p>
+                    <a className='github-link' href="">Github</a>
                 </div>
                 <div className='project-details'>
-                    <p>Hotel Ranking System Sep 2023 – Dec 2023</p>
-                    <p>Technology: Java, JSoup, Selenium</p>
-                    <p>Created a hotel recommendation system empowering users to evaluate options based on amenities, location, and public reviews, enhancing travel decisions</p>
-                    <p>Developed a engaging dashboard by parsing 3 sites to show results and included features such as spell checking and the most searched hotels</p>
+                    <p className='project-heading'>Hotel Ranking System (Sep 2023 - Dec 2023)</p>
+                    <p className='project-tech'>Technology: Java, JSoup, Selenium</p>
+                    <p className='project-description'>Created a hotel recommendation system empowering users to evaluate options based on amenities, location, and public reviews, enhancing travel decisions</p>
+                    <p className='project-description'>Developed a engaging dashboard by parsing 3 sites to show results and included features such as spell checking and the most searched hotels</p>
+                    <a className='github-link' href="">Github</a>
                 </div>
                 <div className='project-details'>
-                    <p>Regression Analysis Tool Jan 2023 – Jun 2023</p>
-                    <p>Technology: Python, MongoDB</p>
-                    <p>Wrote a solution to automate multiple test cases, primarily used to reduce the time required for regression testing</p>
-                    <p>Achieved a 40% enhancement in project delivery by reducing time spent writing regression test cases</p>
+                    <p className='project-heading'>Regression Analysis Tool (Jan 2023 - Jun 2023)</p>
+                    <p className='project-tech'>Technology: Python, MongoDB</p>
+                    <p className='project-description'>Wrote a solution to automate multiple test cases, primarily used to reduce the time required for regression testing</p>
+                    <p className='project-description'> Achieved a 40% enhancement in project delivery by reducing time spent writing regression test cases</p>
+                    <a className='github-link' href="">Github</a>
                 </div>
             </div>
         </div>
